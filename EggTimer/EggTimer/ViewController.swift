312d84d1ec3f56188eeeb6d65bc6d4309ae2b0f8
순진이 <sail041@naver.com>
2022-02-05 19:07:56 +0900

    complete UI

--- EggTimer/EggTimer/ViewController.swift
+++ EggTimer/EggTimer/ViewController.swift
@@ -12,15 +12,23 @@ class ViewController: UIViewController {
     let mainLbl = UILabel()
     let background = UIImageView()
     
+    let softBtn = UIButton()
     let softLbl = UILabel()
-    let softBtn = UIButton(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
     let mediumBtn = UIButton()
     let mediumLbl = UILabel()
     let hardBtn = UIButton()
     let hardLbl = UILabel()
     
+    let progressBar = UIProgressView()
+    
     let pointColor = UIColor(red: 81.0 / 255.0, green: 215.0 / 255.0, blue: 202.0 / 255.0, alpha: 1.0)
     let pointColor2 = UIColor(red: 95.0 / 255.0, green: 87.0 / 255.0, blue: 215.0 / 255.0, alpha: 1.0)
+    
+    var timer = Timer()
+    var totalTime = 0
+    var timePassed = 0
+    var hardness = 0
+    let timeToBoil = ["반숙":2, "반+완숙":3, "완숙":4]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,6 +37,37 @@ class ViewController: UIViewController {
 
 
 }
+
+//MARK: -Event
+extension ViewController {
+    @objc func BtnTapped(_ sender: UIButton) {
+        progressBar.progress = 0.0
+        hardness = timeToBoil[sender]
+        print(hardness)
+        
+        switch sender {
+        case softBtn:
+            hardness = timeToBoil["반숙"]
+        case mediumBtn:
+            print("medium")
+        case hardBtn:
+            print("hard")
+        default:
+            break
+        }
+        
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
+        
+        
+    }
+    
+    @objc func updateTime() {
+        //let percentageTimer = timePassed / totalTime
+        //print(percentageTimer)
+    }
+
+}
+
 
 //MARK: -UI
 extension ViewController {
@@ -46,6 +85,7 @@ extension ViewController {
         
         background.image = UIImage(named: "cool-background")
         
+        
         softBtn.setImage(UIImage(named: "soft_egg"), for: .normal)
         softLbl.text = "반숙"
         
@@ -55,19 +95,24 @@ extension ViewController {
         hardBtn.setImage(UIImage(named: "hard_egg"), for: .normal)
         hardLbl.text = "완숙"
         
-//        [softBtn, mediumBtn, hardBtn].forEach {
-//            $0.contentMode = .scaleToFill
-//        }
+        [softBtn, mediumBtn, hardBtn].forEach {
+            $0.contentMode = .scaleAspectFit
+        }
         
         [softLbl, mediumLbl, hardLbl].forEach {
             $0.textAlignment = .center
             $0.textColor = pointColor
-            $0.font = UIFont.boldSystemFont(ofSize: 30)
+            $0.font = UIFont.boldSystemFont(ofSize: 25)
         }
+        progressBar.backgroundColor = .white
+        progressBar.progress = 0.5
+        progressBar.tintColor = pointColor2
     }
     
     final private func addTarget() {
-        
+        softBtn.addTarget(self, action: #selector(BtnTapped(_:)), for: .touchUpInside)
+        mediumBtn.addTarget(self, action: #selector(BtnTapped(_:)), for: .touchUpInside)
+        hardBtn.addTarget(self, action: #selector(BtnTapped(_:)), for: .touchUpInside)
     }
     
     final private func setConstraints() {
@@ -89,29 +134,36 @@ extension ViewController {
         stackView.spacing = 20
 
 
-        [background, mainLbl, stackView].forEach {
+        [background, mainLbl, stackView, progressBar].forEach {
             view.addSubview($0)
             $0.translatesAutoresizingMaskIntoConstraints = false
         }
         
         NSLayoutConstraint.activate([
+            progressBar.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -200),
+            progressBar.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            progressBar.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            //progressBar.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            
             background.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
             background.topAnchor.constraint(equalTo: view.topAnchor),
             background.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
             background.bottomAnchor.constraint(equalTo: view.bottomAnchor),
 
-
-            stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10),
-            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 250),
-            stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -10),
-            //stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -100),
             
-            softBtn.widthAnchor.constraint(equalToConstant: 180),
-            softStack.heightAnchor.constraint(equalToConstant: 180),
+            mediumLbl.heightAnchor.constraint(equalTo: softLbl.heightAnchor),
+            hardLbl.heightAnchor.constraint(equalTo: softLbl.heightAnchor),
+
+            
+            stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10),
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 240),
+            stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -10),
+            stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -300),
+            
             
             mainLbl.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
-            //mainLbl.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor),
-            mainLbl.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 80),
+            mainLbl.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 120),
+            
             
         ])
     }
